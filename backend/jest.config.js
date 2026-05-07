61464f3e277a21ac3ca905af3af0d2a9a7237215
Pedro Heleno <phbarros99@gmail.com>
2026-05-06 22:32:04 -0300

    feat: adição do supertest e configurações do jest

---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['./tests/setup.js'],
+    testMatch: ['**/tests/**/*.test.js'],
+    collectCoverageFrom: [
+        'controllers/**/*.js',
+        'services/**/*.js',
+        '!**/node_modules/**',
+    ],
+};
